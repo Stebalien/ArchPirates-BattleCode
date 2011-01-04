382e76c9b3ac860c9be5838bcc5aed70bee1b5a3
Steven Allen <steven@stebalien.com>
2011-01-04 16:32:22 -0500

    Initial commit

---
+++ archpirates/RobotPlayer.java
@@ -0,0 +1,67 @@
+package archpirates;
+
+import battlecode.common.*;
+import static battlecode.common.GameConstants.*;
+
+public class RobotPlayer implements Runnable {
+
+	private final RobotController myRC;
+
+    public RobotPlayer(RobotController rc) {
+        myRC = rc;
+    }
+
+	public void run() {
+		ComponentController [] components = myRC.newComponents();
+		System.out.println(java.util.Arrays.toString(components));
+		System.out.flush();
+		if(myRC.getChassis()==Chassis.BUILDING)
+			runBuilder((MovementController)components[0],(BuilderController)components[2]);
+		else
+			runMotor((MovementController)components[0]);
+	}
+
+	public void testit(MovementController m) {
+		m.withinRange(myRC.getLocation());
+	}
+
+	public void runBuilder(MovementController motor, BuilderController builder) {
+		while (true) {
+            try {
+				myRC.yield();
+
+				if(!motor.canMove(myRC.getDirection()))
+					motor.setDirection(myRC.getDirection().rotateRight());
+				else if(myRC.getTeamResources()>=2*Chassis.LIGHT.cost)
+					builder.build(Chassis.LIGHT,myRC.getLocation().add(myRC.getDirection()));
+
+            } catch (Exception e) {
+                System.out.println("caught exception:");
+                e.printStackTrace();
+            }
+        }
+	}
+
+    public void runMotor(MovementController motor) {
+        while (true) {
+            try {
+                /*** beginning of main loop ***/
+                while (motor.isActive()) {
+                    myRC.yield();
+                }
+
+                if (motor.canMove(myRC.getDirection())) {
+                    //System.out.println("about to move");
+                    motor.moveForward();
+                } else {
+                    motor.setDirection(myRC.getDirection().rotateRight());
+                }
+
+                /*** end of main loop ***/
+            } catch (Exception e) {
+                System.out.println("caught exception:");
+                e.printStackTrace();
+            }
+        }
+    }
+}
