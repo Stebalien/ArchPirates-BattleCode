3eb32462f1d4326474df501f3002c8768af86f97
Steven Allen <steven@stebalien.com>
2011-01-10 02:35:18 -0500

    Added commented scout code to the end of the scout class.

--- archpirates/modules/castes/Scout.java
+++ archpirates/modules/castes/Scout.java
@@ -61,3 +61,75 @@ public class Scout extends Caste {
         // TODO
     }
 }
+/*
+    pubblic void scout_start() {
+        nav.setDestination(new MapLocation(0, 0));
+        SensorController sensor = null;
+        BuilderController builder = null;
+        for(ComponentController contr: myRC.components()) {
+            ComponentClass c = contr.componentClass();
+            if(c == ComponentClass.SENSOR) {
+                sensor = (SensorController)contr;
+            } else if(c == ComponentClass.BUILDER) {
+                builder = (BuilderController)contr;
+            }
+        }
+
+        Mine[] targets = new Mine[10];
+        int ti = -1;
+        boolean building = false;
+
+        state = wander;
+    }
+
+    public void scout_wander() {
+        if(ti < 0) {
+            Mine[] mines = sensor.senseNearbyGameObjects(Mine.class);
+            for(Mine m: mines) {
+                if(ti < 9 && sensor.senseMineInfo(m).roundsLeft == GameConstants.MINE_ROUNDS) {
+                    targets[++ti] = m;
+                    nav.setDestination(m.getLocation(), 1.5);
+                }
+            }
+        }
+
+        if(ti < 0) {
+            //wander
+            nav.bugNavigate();
+        } else if(nav.bugNavigate()) {
+                    MapLocation mineLoc = targets[ti].getLocation();
+
+                    if(mineLoc.equals(myRC.getLocation())) {
+                        if(nav.canMoveBackward())
+                            nav.move(false);
+                    } else if(!building) {
+                        System.out.println("Building a mine");
+                        if(!builder.isActive() && myRC.getTeamResources() >= 1.2*Chassis.BUILDING.cost) {
+                            builder.build(Chassis.BUILDING, targets[ti].getLocation());
+                            building = true;
+                        }
+                    } else {
+                        System.out.println("Building recycler...");
+                        if(!builder.isActive() && myRC.getTeamResources() >= 1.2*ComponentType.RECYCLER.cost) {
+                            builder.build(ComponentType.RECYCLER, targets[ti].getLocation(), RobotLevel.ON_GROUND);
+                            building = false;
+                            ti--;
+
+                            if(ti > -1)
+                                nav.setDestination(targets[ti].getLocation(), 1.9);
+                            else
+                                nav.setDestination(new MapLocation(0, 0));
+                        }
+                    }
+                }
+
+            } catch (Exception e) {
+                System.out.println("caught exception:");
+                e.printStackTrace();
+            }
+
+            System.out.println(Clock.getBytecodeNum());
+            myRC.yield();
+        }
+    }
+*/ 
