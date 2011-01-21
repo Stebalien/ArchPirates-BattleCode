21c51781bf670abde4004b52ff0205e835579767
Steven Allen <steven@stebalien.com>
2011-01-20 23:42:56 -0500

    Always fire on the closest visible robot (rescan/switch target). This means that we always stay as far away as possible.

--- team094/modules/Attacker.java
+++ team094/modules/Attacker.java
@@ -23,7 +23,6 @@ public class Attacker {
     private Robot [] robots;
     private double hp;
     private MapLocation runDest;
-    private Robot robot;
     private boolean fired; // Only scan if we have not fired recently.
     private MapLocation lastLoc;
     // }}}
@@ -69,7 +68,6 @@ public class Attacker {
             for (Chassis c: chassis) {
                 this.chassisMask |= (1 << c.ordinal());
             }
-            robot = null; // No longer store robotInfo.
         }
     }
 
@@ -81,41 +79,10 @@ public class Attacker {
     public void setTeam(Team team) {
         if (this.team != team) {
             this.team = team;
-            robot = null;
         }
     }
     // }}}
 
-    // {{{ Set Target
-    /**
-     * Sets the target.
-     *
-     * @param robot The robot to target.
-     * @return true if the target is within sensor range.
-     */
-    public boolean setTarget(Robot robot) {
-        if (sensor.canSenseObject(robot)) {
-            this.robot = robot;
-            return true;
-        }
-        return false;
-    }
-
-    /**
-     * Sets the target.
-     *
-     * @param location The location to target.
-     * @param level The level of the target.
-     * @return true if the target is within sensor range.
-     */
-    public boolean setTarget(MapLocation location, RobotLevel level) throws GameActionException {
-        if (sensor.canSenseSquare(location)) {
-            robot = (Robot)sensor.senseObjectAtLocation(location, level);
-            return true;
-        }
-        return false;
-    }
-    // }}}
 
     // {{{ Fire At
     /**
@@ -160,27 +127,21 @@ public class Attacker {
 
         int tmpDistSq, minDistSq = 1000;
 
-        if (fired && robot != null && sensor.canSenseObject(robot)) {
-            robotInfo = sensor.senseRobotInfo(robot);
-        } else {
-            robots = sensor.senseNearbyGameObjects(Robot.class);
-            robot = null; // will be overwritten anyway but we DEFINITELY don't want this as not null if it is invalid.
+        robots = sensor.senseNearbyGameObjects(Robot.class);
 
-            // Find a new target
-            for (Robot r : robots) {
-                if (team != r.getTeam())
-                    continue;
-                tmpRobotInfo = sensor.senseRobotInfo(r);
+        // Find a new target
+        for (Robot r : robots) {
+            if (team != r.getTeam())
+                continue;
+            tmpRobotInfo = sensor.senseRobotInfo(r);
 
-                rank += tmpRobotInfo.chassis.cost;
-                mask |= (bit = (1 << tmpRobotInfo.chassis.ordinal()));
-                if (chassisMask == 0 || (chassisMask & bit) != 0)
-                {
-                    if ((tmpDistSq = myLoc.distanceSquaredTo(tmpRobotInfo.location)) < minDistSq) {
-                        minDistSq = tmpDistSq;
-                        robotInfo = tmpRobotInfo;
-                        robot = r;
-                    }
+            rank += tmpRobotInfo.chassis.cost;
+            mask |= (bit = (1 << tmpRobotInfo.chassis.ordinal()));
+            if (chassisMask == 0 || (chassisMask & bit) != 0)
+            {
+                if ((tmpDistSq = myLoc.distanceSquaredTo(tmpRobotInfo.location)) < minDistSq) {
+                    minDistSq = tmpDistSq;
+                    robotInfo = tmpRobotInfo;
                 }
             }
         }
@@ -213,7 +174,7 @@ public class Attacker {
             if (!gun.isActive()) {
                 try {
                     if (gun.withinRange(robotInfo.location)) {
-                        gun.attackSquare(robotInfo.location, robot.getRobotLevel());
+                        gun.attackSquare(robotInfo.location, robotInfo.robot.getRobotLevel());
                         fired = true;
                     }
                 } catch(GameActionException e) {e.printStackTrace();}
@@ -235,25 +196,18 @@ public class Attacker {
      */
     public MapLocation autoFire(WeaponController weapon) throws GameActionException {
         RobotInfo robotInfo = null, tmpRobotInfo = null;
+        robots = sensor.senseNearbyGameObjects(Robot.class);
 
-        if (fired && robot != null && sensor.canSenseObject(robot)) {
-            robotInfo = sensor.senseRobotInfo(robot);
-        } else {
-            robots = sensor.senseNearbyGameObjects(Robot.class);
-            robot = null; // will be overwritten anyway but we DEFINITELY don't want this as not null if it is invalid.
-
-            // Find a new target
-            for (Robot r : robots) {
-                if (team != r.getTeam())
-                    continue;
-                tmpRobotInfo = sensor.senseRobotInfo(r);
-                if ((chassisMask == 0 || (chassisMask & (1 << tmpRobotInfo.chassis.ordinal())) != 0)) 
-                {
-                    robotInfo = tmpRobotInfo;
-                    robot = r;
-                    if (weapon.withinRange(robotInfo.location)) {
-                        break;
-                    }
+        // Find a new target
+        for (Robot r : robots) {
+            if (team != r.getTeam())
+                continue;
+            tmpRobotInfo = sensor.senseRobotInfo(r);
+            if ((chassisMask == 0 || (chassisMask & (1 << tmpRobotInfo.chassis.ordinal())) != 0)) 
+            {
+                robotInfo = tmpRobotInfo;
+                if (weapon.withinRange(robotInfo.location)) {
+                    break;
                 }
             }
         }
@@ -267,7 +221,7 @@ public class Attacker {
         } else if (!weapon.isActive()) {
             try {
                 if (weapon.withinRange(robotInfo.location)) {
-                    weapon.attackSquare(robotInfo.location, robot.getRobotLevel());
+                    weapon.attackSquare(robotInfo.location, robotInfo.robot.getRobotLevel());
                     fired = true;
                 }
             } catch(GameActionException e) {e.printStackTrace();}
